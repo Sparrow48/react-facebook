20e3d0c3ffd2cea107c716c6b7dafcf873c8a828
Sparrow48 <abunasib48@gmail.com>
2021-05-30 19:23:39 +0600

    Done create room section

---
+++ src/components/MainBody/ActiveUsers/ActiveUser.js
@@ -0,0 +1,51 @@
+import React from "react";
+import classes from "./ActiveUser.module.css";
+import { BiVideoPlus } from "react-icons/bi";
+import ProfilePic from "../../../assets/nasib2.png";
+function CreateRoom() {
+  return (
+    <div className={classes.activeUsers}>
+      <div className={classes.createRoom}>
+        <BiVideoPlus className={classes.createRoomIcon} />
+        <p>Create Room</p>
+      </div>
+      <div className={classes.activeUserList}>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+        <div className={classes.activeUser}>
+          <img src={ProfilePic} alt='Profile_Image'></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateRoom;
--- src/components/MainBody/MainBody.js
+++ src/components/MainBody/MainBody.js
@@ -2,12 +2,14 @@ import classes from "./MainBody.module.css";
 import React from "react";
 import Story from "./StorySection/Story";
 import CreatePost from "./PostSection/CreatePost";
+import ActiveUsers from "./ActiveUsers/ActiveUser";
 
 function MainBody() {
   return (
     <div className={classes.mainBody}>
       <Story />
       <CreatePost />
+      <ActiveUsers />
     </div>
   );
 }
